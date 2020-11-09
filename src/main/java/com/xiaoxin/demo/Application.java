2617755ee4e0b8c487ab8eb50f6e01ad85cc9a02
axin <2190237715@qq.com>
2020-11-09 09:18:48 +0800

    生产配置

--- src/main/java/com/xiaoxin/demo/Application.java
+++ src/main/java/com/xiaoxin/demo/Application.java
@@ -3,6 +3,8 @@ package com.xiaoxin.demo;
 import com.xiaoxin.demo.util.PortUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,11 +13,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableElasticsearchRepositories(basePackages = "com.xiaoxin.demo.search")
 @EnableJpaRepositories(basePackages = {"com.xiaoxin.demo.dao", "com.xiaoxin.demo.pojo"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
     static {
         PortUtil.checkPort(6379, "Redis 服务端", true);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
